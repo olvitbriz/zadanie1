61f67934c619795157c3c0e6425df62981e684e4
Olia Brizgina <olvitbriz@gmail.com>
2023-01-05 16:16:56 +0400

    initial commit

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        int price = 4523;
+        int bonusMile = 20;
+        int countBonusMile = price / bonusMile;
+        System.out.println( "Количество бонусных миль " + countBonusMile);
+    }
+}
